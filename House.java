3c3b5096d2a0b034a57bc517d5d9d021c40c96ff
Caitlyn Kim <cakim@smith.edu>
2025-03-27 01:06:46 -0400

    initalized attributes in House's constructor

--- House.java
+++ House.java
@@ -1,8 +1,24 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building {
+  
+  private ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  
+  public House(boolean hasDiningRoom) {
+    this.residents = new ArrayList<Student>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
+  }
+
+  public boolean hasDiningRoom(){
+
+  }
+  
+  public int nResidents(){
+
   }
 
   public static void main(String[] args) {
